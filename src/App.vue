3449956449ae86a5692f2a470eee1a733f1ad32d
rpererah <rpererah@gmail.com>
2024-03-21 12:01:17 -0300

    primeiros passos com vue

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+import Banner from './components/Banner.vue';
+export default{
+    components:{Banner}
+}
+</script>
+
+<template>
+ <Banner/>
+</template>
+
+<!-- a palavra scoped referece que mesmo que eu tenha classes class
+com nomes iguais elas nao se afetaram, pois com scoped somente
+o conteudo da propria pagina com aquele nome css sera afetado,
+por exemplo se tanto em banner como aqui eu tiver uma class
+com nome exemplo, cada uma vai seguir o seu estilo -->
+<style scoped>
+</style>
+
